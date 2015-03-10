121f8097c12a35928a95e042e0b275f6f8a4df66
jeffreyz <jeffreyz@jeffreyz.q-free.com>
2015-03-10 14:12:34 +0100

    Minimal Spring integration with XML config.
    BIRT Viewer still works.

---
+++ birt-viewer/src/main/java/com/qfree/obo/report/rest/server/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.qfree.obo.report.rest.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class JAXRSConfiguration extends Application {
+
+}
