834ee7c049d5bf6dcd3516c108e7ec8bb6dd6cb8
jeffreyz <jeffreyz@jeffreyz.q-free.com>
2015-03-17 18:17:04 +0100

    Added class CreateCloseApplicationContext to test generating db tables and fixture data.
    Savepoint created so that I can start renaming classes, methids, etc.:
    
    	Spittle -> Report
    	Spitter	-> ReportCategory
    
    as well as *many* other changes to match these changes.

--- birt-viewer/src/main/java/com/qfree/obo/report/db/SpringDataJpaConfig.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/db/SpringDataJpaConfig.java
@@ -1,4 +1,5 @@
 package com.qfree.obo.report.db;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -10,39 +11,41 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+// TODO ***** IS THIS CONFIGURATION CLASS NEEDED AT ALL????????????????????? *****
+
 @Configuration
 @EnableJpaRepositories(basePackages = "com.qfree.obo.report.db")
 public class SpringDataJpaConfig {
-  
-  @Bean
-  public DataSource dataSource() {
-    return new EmbeddedDatabaseBuilder()
-				.addScript("classpath:/db/schema.sql")
-				.addScript("classpath:/db/test-data.sql")
-        .build();
-  }
-  
-  @Bean
-  public JpaTransactionManager transactionManager() {
-    return new JpaTransactionManager(); // does this need an emf???
-  }
-  
-  @Bean
-  public HibernateJpaVendorAdapter jpaVendorAdapter() {
-    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-    adapter.setDatabase(Database.H2);
-    adapter.setShowSql(false);
-    adapter.setGenerateDdl(true);
-    return adapter;
-  }
-  
-  @Bean
-  public Object emf() {
-    LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-    emf.setDataSource(dataSource());
-    emf.setPersistenceUnitName("spitter");
-    emf.setJpaVendorAdapter(jpaVendorAdapter());
-    return emf;
-  }
-  
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.addScript("classpath:db/schema.sql")
+				.addScript("classpath:db/test-data.sql")
+				.build();
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		return new JpaTransactionManager(); // does this need an emf???
+	}
+
+	@Bean
+	public HibernateJpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.H2);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(true);
+		return adapter;
+	}
+
+	@Bean
+	public Object emf() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		emf.setPersistenceUnitName("reportServer");
+		emf.setJpaVendorAdapter(jpaVendorAdapter());
+		return emf;
+	}
+
 }
--- birt-viewer/src/test/java/com/qfree/obo/report/db/JpaConfig.java
+++ birt-viewer/src/test/java/com/qfree/obo/report/db/JpaConfig.java
@@ -4,8 +4,11 @@ import javax.inject.Inject;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -20,17 +23,40 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.qfree.obo.report.db")
+@PropertySource("classpath:config.properties")
+//This is for *multiple* properties files (Spring 4+). The @PropertySource 
+//elements must be comma-separated:
+//@PropertySources({
+//		@PropertySource("classpath:config.properties")
+//})
 public class JpaConfig {
+	/*
+	 * The injected "env" object here will contain key/value pairs for each 
+	 * property in the properties files specified above in the @PropertySource
+	 * or @PropertySources annotation above.
+	 */
+	@Autowired
+	private Environment env;
 
-  @Bean
-  public DataSource dataSource() {
-    EmbeddedDatabaseBuilder edb = new EmbeddedDatabaseBuilder();
-    edb.setType(EmbeddedDatabaseType.H2);
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder edb = new EmbeddedDatabaseBuilder();
+		edb.setType(EmbeddedDatabaseType.H2);
 		edb.addScript("classpath:db/schema.sql");
 		edb.addScript("classpath:db/test-data.sql");
-    EmbeddedDatabase embeddedDatabase = edb.build();
-    return embeddedDatabase;
-  }
+		EmbeddedDatabase embeddedDatabase = edb.build();
+		return embeddedDatabase;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.H2);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+		return adapter;
+	}
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean
@@ -42,17 +68,15 @@ public class JpaConfig {
 		emf.setPackagesToScan("com.qfree.obo.report.domain");
 		return emf;
 	}
-  
-  @Bean
-  public JpaVendorAdapter jpaVendorAdapter() {
-    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-    adapter.setDatabase(Database.H2);
-    adapter.setShowSql(true);
-    adapter.setGenerateDdl(false);
-    adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
-    return adapter;
-  }
-  
+
+	//	Properties additionalProperties() {
+	//		Properties properties = new Properties();
+	//		properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+	//		properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+	//		properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+	//
+	//		return properties;
+	//	}
 
   @Configuration
   @EnableTransactionManagement
@@ -68,5 +92,4 @@ public class JpaConfig {
       return transactionManager;
     }    
   }
-  
 }
