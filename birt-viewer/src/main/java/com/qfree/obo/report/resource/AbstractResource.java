cbf56654fa293e023d32fbfd2ea427882741461c
jeffreyz <jeffreyz@jeffreyz.q-free.com>
2015-04-26 00:14:41 +0200

    Savepoint after Improving/refactoring AbstractResource.java and ReportResource.java. It should be possible to easily manage the "href" attribute for all resource classes now.
    Time to use AbstractResource with other resource classes now.

--- birt-viewer/src/main/java/com/qfree/obo/report/resource/AbstractResource.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/resource/AbstractResource.java
@@ -1,7 +1,101 @@
 package com.qfree.obo.report.resource;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * An "xsi:type" JSON attribute normally appears whenever an instance of a 
+ * subclass is marshalled. This @XmlTransient annotation applied at the class 
+ * level stops that element from being generated.
+ */
+@XmlTransient
 public abstract class AbstractResource {
 
-	public static final String PATH_SEPARATOR = "/";
+	private static final Logger logger = LoggerFactory.getLogger(AbstractResource.class);
+
+	//	public static final String PATH_SEPARATOR = "/";
+
+	@XmlElement
+	protected String href;
+
+	public AbstractResource() {
+		super();
+	}
+
+	public AbstractResource(UriInfo uriInfo, Class<?> entityClass, Object id) {
+		super();
+		//		this.href = createHref(getFullyQualifiedContextPath(uriInfo), entityClass, id);
+		this.href = createHref(uriInfo, entityClass, id);
+	}
+
+	//	public String getHref() {
+	//		return href;
+	//	}
+	//
+	//	public void setHref(String href) {
+	//		this.href = href;
+	//	}
+
+	protected static String createHref(UriInfo uriInfo, Class<?> entityClass, Object id) {
+		logger.info("entityClass.getName() = {}", entityClass.getName());
+		/*
+		 * The fully-qualified path is obtained from the UriInfo object that is
+		 * passed as a parameter to a JAX-RS controller method using:
+		 * 
+		 *     Context UriInfo uriInfo}
+		 * 
+		 * It includes the entire request URL except for path components that  
+		 * are specific to an endpoint. For example, this could be:
+		 * 
+		 *     http://localhost:8080/report-server/rest/
+		 */
+		String fqBasePath = uriInfo.getBaseUri().toString();
+		logger.info("fqBasePath 1 = {}", fqBasePath);
+		if (fqBasePath.endsWith("/")) {
+			fqBasePath = fqBasePath.substring(0, fqBasePath.length() - 1);
+			logger.info("fqBasePath 2 = {}", fqBasePath);
+		}
+		//		String fqBasePath = getFullyQualifiedContextPath(uriInfo);
+
+		//		StringBuilder sb = new StringBuilder(fqBasePath);
+		ResourcePath resourcePath = ResourcePath.forEntity(entityClass);
+		logger.info("resourcePath = {}", resourcePath);
+		//		sb.append(resourcePath.getPath()).append(PATH_SEPARATOR).append(id);
+		Path p1 = Paths.get(fqBasePath, resourcePath.getPath(), id.toString());
+		logger.info("p1.toString() = {}", p1.toString());
+		//		logger.info("sb.toString() = {}", sb.toString());
+		//		return sb.toString();
+		return p1.toString();
+	}
+
+	//	/**
+	//	 * Returns the fully-qualified path given the UriInfo object passed as a 
+	//	 * parameter to a JAX-RS controller method using:<p>
+	//	 * <p>
+	//	 * {@code Context UriInfo uriInfo}<p>
+	//	 * <p>
+	//	 * It includes the entire request URL except for path components that are 
+	//	 * specific to an endpoint. For example, this could be:<p>
+	//	 * <p>
+	//	 * http://localhost:8080/report-server/rest/
+	//	 * 
+	//	 * @param uriInfo
+	//	 * @return
+	//	 */
+	//	protected static String getFullyQualifiedContextPath(UriInfo uriInfo) {
+	//		String fqBasePath = uriInfo.getBaseUri().toString();
+	//		logger.info("fq = {}", fqBasePath);
+	//		if (fqBasePath.endsWith("/")) {
+	//			return fqBasePath.substring(0, fqBasePath.length() - 1);
+	//		}
+	//		return fqBasePath;
+	//	}
 
 }
--- birt-viewer/src/main/java/com/qfree/obo/report/resource/ReportResource.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/resource/ReportResource.java
@@ -1,9 +1,6 @@
 package com.qfree.obo.report.resource;
 
-import static com.qfree.obo.report.resource.AbstractResource.PATH_SEPARATOR;
-
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 import javax.ws.rs.core.UriInfo;
@@ -14,16 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.qfree.obo.report.domain.Report;
-import com.qfree.obo.report.domain.ReportVersion;
-import com.qfree.obo.report.domain.RoleReport;
 
 @XmlRootElement
-public class ReportResource {
+public class ReportResource extends AbstractResource {
 
 	private static final Logger logger = LoggerFactory.getLogger(ReportResource.class);
-
-	@XmlElement
-	private String href;
 
 	@XmlElement
 	private UUID reportId;
@@ -54,64 +46,55 @@ public class ReportResource {
 	}
 
 	public ReportResource(UriInfo uriInfo, Report report) {
-		this(createHref(getFullyQualifiedContextPath(uriInfo), Report.class, report.getReportId()), report);
+
+		super(uriInfo, Report.class, report.getReportId());
+
+		this.reportId = report.getReportId();
+		//				this.reportCategoryResource = new ReportCategoryResource(uriInfo, report.getReportCategory());
+		this.name = report.getName();
+		this.number = report.getNumber();
+		//		this.reportVersions = report.getReportVersions();
+		//		this.roleReports = report.getRoleReports();
+		this.active = report.isActive();
+		this.createdOn = report.getCreatedOn();
+		//		this(createHref(getFullyQualifiedContextPath(uriInfo), Report.class, report.getReportId()), report);
 	}
 
-	public ReportResource(String href, Report report) {
-		this(
-				href,
-				report.getReportId(),
-				new ReportCategoryResource(report.getReportCategory()),
-				report.getName(),
-				report.getNumber(),
-				report.getReportVersions(),
-				report.getRoleReports(),
-				report.isActive(),
-				report.getCreatedOn());
-	}
-
-	public ReportResource(String href, UUID reportId, ReportCategoryResource reportCategoryResource, String name,
-			Integer number,
-			List<ReportVersion> reportVersions, List<RoleReport> roleReports, boolean active, Date createdOn) {
-
-		logger.info("href = {}", href);
-		this.href = href;
-
-		logger.info("reportId = {}", reportId);
-		this.reportId = reportId;
-		//		this.reportCategoryResource = reportCategoryResource;
-		this.name = name;
-		this.number = number;
-		//		this.reportVersions = reportVersions;
-		//		this.roleReports = roleReports;
-		this.active = active;
-		this.createdOn = createdOn;
-	}
+	//	public ReportResource(String href, Report report) {
+	//		//		public ReportResource(Report report) {
+	//		this(
+	//				href,
+	//				report.getReportId(),
+	//				new ReportCategoryResource(report.getReportCategory()),
+	//				report.getName(),
+	//				report.getNumber(),
+	//				report.getReportVersions(),
+	//				report.getRoleReports(),
+	//				report.isActive(),
+	//				report.getCreatedOn());
+	//	}
+	//
+	//	public ReportResource(String href, UUID reportId, ReportCategoryResource reportCategoryResource, String name,
+	//			Integer number,
+	//			List<ReportVersion> reportVersions, List<RoleReport> roleReports, boolean active, Date createdOn) {
+	//
+	//		logger.info("href = {}", href);
+	//		this.href = href;
+	//
+	//		logger.info("reportId = {}", reportId);
+	//		this.reportId = reportId;
+	//		//		this.reportCategoryResource = reportCategoryResource;
+	//		this.name = name;
+	//		this.number = number;
+	//		//		this.reportVersions = reportVersions;
+	//		//		this.roleReports = roleReports;
+	//		this.active = active;
+	//		this.createdOn = createdOn;
+	//	}
 
 	@Override
 	public String toString() {
 		return "ReportResource [href=" + href + "]";
-	}
-
-	//PLACE IN ABSTRACT SUPERCLASS???:
-	//SUPERCLASS MUST ALSO HAVE AN "href" field, instead of explicitly declaring it at the top of this class???????????????
-	//CAN WE THE SET THE "href"	CLASS IN THE CONSTRUCTOR OF THAT SUPERCLASS?
-	//IF THIS WORKS, I MAY HAVE TO INCLUDE "Uuid" IN THE NAME OF THE CLASS SO THAT WE CAN ALSO HAVE ONE THAT WORKS WITH "Bigserial" ids????
-
-	protected static String getFullyQualifiedContextPath(UriInfo info) {
-		String fq = info.getBaseUri().toString();
-		logger.info("fq = {}", fq);
-		if (fq.endsWith("/")) {
-			return fq.substring(0, fq.length() - 1);
-		}
-		return fq;
-	}
-
-	protected static String createHref(String fqBasePath, Class<?> entityClass, Object id) {
-		StringBuilder sb = new StringBuilder(fqBasePath);
-		ResourcePath resourcePath = ResourcePath.forEntity(entityClass);
-		sb.append(resourcePath.getPath()).append(PATH_SEPARATOR).append(id);
-		return sb.toString();
 	}
 
 }
