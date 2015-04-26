26dd661acd7d3988901280cdf1f4e9d5f703590d
jeffreyz <jeffreyz@jeffreyz.q-free.com>
2015-04-26 11:20:49 +0200

    Cleaned up ConfigurationResource & ConfigurationController to use the same code structure as the other Resources & Controllers.

--- birt-viewer/src/main/java/com/qfree/obo/report/resource/ConfigurationResource.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/resource/ConfigurationResource.java
@@ -3,18 +3,18 @@ package com.qfree.obo.report.resource;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.qfree.obo.report.domain.Configuration;
 import com.qfree.obo.report.domain.Configuration.ParamName;
-import com.qfree.obo.report.domain.Role;
 
 @XmlRootElement
-public class ConfigurationResource {
+public class ConfigurationResource extends AbstractResource {
 
-	@XmlElement
-	private String href;
+	//	@XmlElement
+	//	private String href;
 
 	@XmlElement
 	//(name = "someOverriddenName")
@@ -23,8 +23,8 @@ public class ConfigurationResource {
 	@XmlElement
 	private Date createdOn;
 
-	@XmlElement
-	private Role role;
+	//	@XmlElement
+	//	private RoleResource roleResource;
 
 	@XmlElement
 	private ParamName paramName;
@@ -38,18 +38,13 @@ public class ConfigurationResource {
 	public ConfigurationResource() {
 	}
 
-	public ConfigurationResource(UUID configurationId, Date createdOn, Role role, ParamName paramName, String value) {
-		this.configurationId = configurationId;
-		this.createdOn = createdOn;
-		this.role = role;
-		this.paramName = paramName;
-		this.value = value;
-	}
+	public ConfigurationResource(UriInfo uriInfo, Configuration configuration) {
 
-	public ConfigurationResource(Configuration configuration) {
+		super(uriInfo, Configuration.class, configuration.getConfigurationId());
+
 		this.configurationId = configuration.getConfigurationId();
 		this.createdOn = configuration.getCreatedOn();
-		this.role = configuration.getRole();
+		//		this.role = configuration.getRole();
 		this.paramName = configuration.getParamName();
 		/*
 		 * Set "value" from appropriate field based on this.paramName.paramType()????????????????????????????????????????????????????????
@@ -60,6 +55,36 @@ public class ConfigurationResource {
 		 * resources and controller. It might be more obvious then. :-)
 		 */
 		//		this.value = configuration.getStringValue();
+
+	}
+
+	//	public ConfigurationResource(UUID configurationId, Date createdOn, Role role, ParamName paramName, String value) {
+	//		this.configurationId = configurationId;
+	//		this.createdOn = createdOn;
+	//		this.role = role;
+	//		this.paramName = paramName;
+	//		this.value = value;
+	//	}
+	//
+	//	public ConfigurationResource(Configuration configuration) {
+	//		this.configurationId = configuration.getConfigurationId();
+	//		this.createdOn = configuration.getCreatedOn();
+	//		this.role = configuration.getRole();
+	//		this.paramName = configuration.getParamName();
+	//		/*
+	//		 * Set "value" from appropriate field based on this.paramName.paramType()????????????????????????????????????????????????????????
+	//		 * No, set "value from configuration.getStringValue(), but this will not work for data type "bytea", 
+	//		 * so I will need to check this.paramName.paramType() for this case??????????????????????????????????????????????????????????????
+	//		 * 
+	//		 * At the moment, I am not sure how to proceed. Just come back to this when I have fleshed out other
+	//		 * resources and controller. It might be more obvious then. :-)
+	//		 */
+	//		//		this.value = configuration.getStringValue();
+	//	}
+
+	@Override
+	public String toString() {
+		return "ConfigurationResource [href=" + href + "]";
 	}
 
 }
--- birt-viewer/src/main/java/com/qfree/obo/report/rest/server/ConfigurationController.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/rest/server/ConfigurationController.java
@@ -1,21 +1,28 @@
 package com.qfree.obo.report.rest.server;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.qfree.obo.report.domain.Configuration.ParamName;
+import com.qfree.obo.report.db.ConfigurationRepository;
+import com.qfree.obo.report.domain.Configuration;
 import com.qfree.obo.report.resource.AbstractResource;
 import com.qfree.obo.report.resource.ConfigurationResource;
+import com.qfree.obo.report.rest.server.RestUtils.RestApiVersion;
 import com.qfree.obo.report.service.ConfigurationService;
 
 @Component
@@ -24,19 +31,53 @@ public class ConfigurationController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ConfigurationController.class);
 
+	private final ConfigurationRepository configurationRepository;
 	private final ConfigurationService configurationService;
 
 	@Autowired
-	public ConfigurationController(ConfigurationService configurationService) {
+	public ConfigurationController(
+			ConfigurationRepository configurationRepository,
+			ConfigurationService configurationService) {
+		this.configurationRepository = configurationRepository;
 		this.configurationService = configurationService;
 	}
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public ConfigurationResource getConfiguration(@HeaderParam("Accept") String acceptHeader) {
-		return new ConfigurationResource(
-				null, new Date(), null, ParamName.TEST_STRING, "Some bloody value");
+	public List<ConfigurationResource> list(
+			@HeaderParam("Accept") String acceptHeader,
+			@Context UriInfo uriInfo) {
+		RestApiVersion apiVersion = RestUtils.extractAPIVersion(acceptHeader, RestApiVersion.v1);
+
+		List<Configuration> configurations = configurationRepository.findAll();
+		List<ConfigurationResource> configurationResources = new ArrayList<>();
+		for (Configuration configuration : configurations) {
+			configurationResources.add(new ConfigurationResource(uriInfo, configuration));
+		}
+		return configurationResources;
 	}
+
+	//	@GET
+	//	@Produces(MediaType.APPLICATION_JSON)
+	//	public ConfigurationResource getConfiguration(@HeaderParam("Accept") String acceptHeader) {
+	//		return new ConfigurationResource(
+	//				null, new Date(), null, ParamName.TEST_STRING, "Some bloody value");
+	//	}
+
+	@Path("/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ConfigurationResource getOne(
+			@PathParam("id") UUID id,
+			@HeaderParam("Accept") String acceptHeader,
+			@Context UriInfo uriInfo) {
+		RestApiVersion apiVersion = RestUtils.extractAPIVersion(acceptHeader, RestApiVersion.v1);
+
+		Configuration configuration = configurationRepository.findOne(id);
+		ConfigurationResource configurationResource = new ConfigurationResource(uriInfo, configuration);
+		return configurationResource;
+	}
+
 	//	@GET
 	//	@Produces(MediaType.TEXT_PLAIN)
 	//	public String test(@HeaderParam("Accept") String acceptHeader) {
