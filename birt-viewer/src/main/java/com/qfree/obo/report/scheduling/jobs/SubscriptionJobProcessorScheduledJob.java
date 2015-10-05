22389315fe805f1f7cd25a96f01b568bea7c7bd4
jeffreyz <jeffreyz@jeffreyz.q-free.com>
2015-10-05 14:19:53 +0200

    SubscriptionJobProcessorScheduledJob.run() is now triggered AND CAN SEE the new Job created by SubscriptionScheduledJob.run().

--- birt-viewer/src/main/java/com/qfree/obo/report/scheduling/jobs/SubscriptionJobProcessorScheduledJob.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/scheduling/jobs/SubscriptionJobProcessorScheduledJob.java
@@ -21,15 +21,35 @@ public class SubscriptionJobProcessorScheduledJob {
 	//TODO Use "synchronized" here??? Just to be sure we do not process the same Job in two different threads?
 	public void run() {
 
+		/*
+		 * Although this job is triggered periodically according its trigger 
+		 * settings, it is *also* triggered manually from:
+		 * 
+		 *     SubscriptionScheduledJob.run()
+		 * 
+		 * so that this method will almost immediately run to process the Job
+		 * created by SubscriptionScheduledJob.run(), instead of waiting for 
+		 * this method to be run according to its trigger settings. 
+		 * 
+		 * If we do *not* sleep here a short while, this code here seems to run
+		 * before the transaction that wraps SubscriptionScheduledJob.run() is
+		 * committed. As a result, we will not see the new Job here. Testing has 
+		 * shown that waiting here for a mere one second is enough to allow that
+		 * transaction to complete. This is not guaranteed to work because it
+		 * depends on non-deterministic multitasking details of the underlying 
+		 * Java VM. But if this method does *not* see the new Job, all it means
+		 * is that we must wait for the next scheduled run of this method (or 
+		 * for SubscriptionScheduledJob.run() to run for another subscription, 
+		 * since that will also trigger this method).
+		 */
+		try {
+			Thread.sleep(1L * 1000L);
+		} catch (InterruptedException e) {
+			// Do nothing
+		}
+
 		logger.info("********** Write me!");
 		logger.info("jobRepository.count() = {}", jobRepository.count());
-
-		//		try {
-		//			Thread.sleep(30L * 1000L);
-		//		} catch (InterruptedException e) {
-		//			e.printStackTrace();
-		//		}
-		//		logger.info("Running again");
 
 	}
 
