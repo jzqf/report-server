17c313000ca5e4d3b0e6d930db49641df294cf94
jeffreyz <jeffreyz@q-free.com>
2016-01-27 13:07:24 +0100

    Fixed report syncing so that reports are not written to the Eclipse project's "reports" directory during integration tests.
    Improved titles on HTML pages.

--- birt-viewer/src/main/java/com/qfree/obo/report/service/ReportSyncService.java
+++ birt-viewer/src/main/java/com/qfree/obo/report/service/ReportSyncService.java
@@ -67,93 +67,103 @@ public class ReportSyncService {
 		 */
 		List<String> reportsNotCreated = new ArrayList<>();
 
-		try {
-			if (ReportUtils.reportSyncSemaphore.tryAcquire(ReportUtils.MAX_WAIT_ACQUIRE_REPORTSYNCSEMAPHORE,
-					TimeUnit.SECONDS)) {
-				try {
+		/*
+		 * We only sync the files if we detect that there is an appropriate
+		 * directory tree in which they can be written. This avoids writing to
+		 * the Eclipse project directory /birt-viewer/src/main/webapp/reports/ ,
+		 * which we do not want. Among other things, these files will then get
+		 * packaged in the WAR generated from this project. This also avoids 
+		 * writing these files during integration testing, e.g., from
+		 * ReportVersionControllerTests.testUpdateByPut().
+		 */
+		if (ReportUtils.applicationPackagedAsWar()) {
+			try {
+				if (ReportUtils.reportSyncSemaphore.tryAcquire(ReportUtils.MAX_WAIT_ACQUIRE_REPORTSYNCSEMAPHORE,
+						TimeUnit.SECONDS)) {
+					try {
 
-					File reportsDirectory = absoluteContextPath
-							.resolve(ReportUtils.BIRT_VIEWER_WORKING_FOLDER).toFile();
-					if (!reportsDirectory.isDirectory()) {
-						throw new InvalidPathException(reportsDirectory.toString(),
-								"\"reports\" directory does not exist");
-					}
-
-					/*
-					 * Delete all existing "rptdesign" files in the "reports" directory.
-					 */
-					File[] files = reportsDirectory.listFiles(new RptdesignFileFilter());
-					if (files != null) {
-						for (File file : files) {
-							if (file.delete()) {
-								reportsDeleted.add(file.getAbsolutePath());
-								logger.info("Deleted file \"{}\"", file.getAbsolutePath());
-							} else {
-								reportsNotDeleted.add(file.getAbsolutePath());
-								logger.warn("Unable to delete file \"{}\"", file.getAbsolutePath());
-							}
+						File reportsDirectory = absoluteContextPath
+								.resolve(ReportUtils.BIRT_VIEWER_WORKING_FOLDER).toFile();
+						if (!reportsDirectory.isDirectory()) {
+							throw new InvalidPathException(reportsDirectory.toString(),
+									"\"reports\" directory does not exist");
 						}
-					}
 
-					/*
-					* Write out all "rptdesign" files from the report server database 
-					* that correspond to active reports and active report versions. 
-					*/
-					List<Report> reports = null;
-					if (RestUtils.FILTER_INACTIVE_RECORDS
-							&& !(showInactiveReports != null ? showInactiveReports : false)) {
-						reports = reportRepository.findByActiveTrue();
-					} else {
-						reports = reportRepository.findAll();
-					}
-					for (Report report : reports) {
 						/*
-						 * Order the report versions for the report being treated
-						 * by "versionCode". In this way, the older versions are
-						 * written to the file system before the more recent
-						 * versions (assuming that htere are multiple enabled
-						 * reports versions for a report). This is important to
-						 * handle the case where rptdesign file names are *not*
-						 * unique. Under these conditions, this approach will 
-						 * ensure that the most recent version will be accessible.
+						 * Delete all existing "rptdesign" files in the "reports" directory.
 						 */
-						List<ReportVersion> reportVersions = report.getReportVersions();
-						Collections.sort(reportVersions, new ReportVersionsSortByVersionCode());
-						for (ReportVersion reportVersion : reportVersions) {
-							/*
-							 * Only write out active report versions. If a report 
-							 * version has been made inactive then it may no longer 
-							 * work correctly or it may display data that should 
-							 * no longer be viewed.
-							 */
-							if (reportVersion.isActive()) {
-								logger.info("Writing report version = {}", reportVersion);
-								logger.debug("report version = \"{}\", number of bytes = {}",
-										reportVersion.getReportVersionId(), reportVersion.getRptdesign().length());
-								/*
-								 * Write uploaded rptdesign file to the file system of the report 
-								 * server, overwriting a file with the same name, if one exists.
-								 */
-								Path rptdesignFilePath = ReportUtils.writeRptdesignFile(reportVersion,
-										absoluteContextPath.toString());
-								reportsCreated.add(rptdesignFilePath.toAbsolutePath().toString());
+						File[] files = reportsDirectory.listFiles(new RptdesignFileFilter());
+						if (files != null) {
+							for (File file : files) {
+								if (file.delete()) {
+									reportsDeleted.add(file.getAbsolutePath());
+									logger.info("Deleted file \"{}\"", file.getAbsolutePath());
+								} else {
+									reportsNotDeleted.add(file.getAbsolutePath());
+									logger.warn("Unable to delete file \"{}\"", file.getAbsolutePath());
+								}
 							}
 						}
-					}
-				} catch (InvalidPathException e) {
-					throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_REPORT_FOLDER_MISSING, e);
-				} catch (IOException e) {
-					throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC, e);
-				} finally {
-					ReportUtils.reportSyncSemaphore.release();
-				}
-			} else {
-				throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC_NO_PERMIT);
-			}
-		} catch (InterruptedException e) {
-			throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC_INTERRUPT, e);
-		}
 
+						/*
+						* Write out all "rptdesign" files from the report server database 
+						* that correspond to active reports and active report versions. 
+						*/
+						List<Report> reports = null;
+						if (RestUtils.FILTER_INACTIVE_RECORDS
+								&& !(showInactiveReports != null ? showInactiveReports : false)) {
+							reports = reportRepository.findByActiveTrue();
+						} else {
+							reports = reportRepository.findAll();
+						}
+						for (Report report : reports) {
+							/*
+							 * Order the report versions for the report being treated
+							 * by "versionCode". In this way, the older versions are
+							 * written to the file system before the more recent
+							 * versions (assuming that htere are multiple enabled
+							 * reports versions for a report). This is important to
+							 * handle the case where rptdesign file names are *not*
+							 * unique. Under these conditions, this approach will 
+							 * ensure that the most recent version will be accessible.
+							 */
+							List<ReportVersion> reportVersions = report.getReportVersions();
+							Collections.sort(reportVersions, new ReportVersionsSortByVersionCode());
+							for (ReportVersion reportVersion : reportVersions) {
+								/*
+								 * Only write out active report versions. If a report 
+								 * version has been made inactive then it may no longer 
+								 * work correctly or it may display data that should 
+								 * no longer be viewed.
+								 */
+								if (reportVersion.isActive()) {
+									logger.info("Writing report version = {}", reportVersion);
+									logger.debug("report version = \"{}\", number of bytes = {}",
+											reportVersion.getReportVersionId(), reportVersion.getRptdesign().length());
+									/*
+									 * Write uploaded rptdesign file to the file system of the report 
+									 * server, overwriting a file with the same name, if one exists.
+									 */
+									Path rptdesignFilePath = ReportUtils.writeRptdesignFile(reportVersion,
+											absoluteContextPath.toString());
+									reportsCreated.add(rptdesignFilePath.toAbsolutePath().toString());
+								}
+							}
+						}
+					} catch (InvalidPathException e) {
+						throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_REPORT_FOLDER_MISSING, e);
+					} catch (IOException e) {
+						throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC, e);
+					} finally {
+						ReportUtils.reportSyncSemaphore.release();
+					}
+				} else {
+					throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC_NO_PERMIT);
+				}
+			} catch (InterruptedException e) {
+				throw new RestApiException(RestError.INTERNAL_SERVER_ERROR_RPTDESIGN_SYNC_INTERRUPT, e);
+			}
+		}
 		return new ReportSyncResource(reportsDeleted, reportsNotDeleted, reportsCreated, reportsNotCreated);
 	}
 
@@ -174,7 +184,7 @@ public class ReportSyncService {
 		Path rptdesignFilePath = null;
 		/*
 		 * We only write the file if we detect that there is an appropriate
-		 * directory tree in which is can be written.
+		 * directory tree in which it can be written.
 		 */
 		if (ReportUtils.applicationPackagedAsWar()) {
 			try {
--- birt-viewer/src/main/webapp/webcontent/qfree/js/jeff.js
+++
@@ -1,2 +0,0 @@
-// This is a comment
-var somevarfromjeffjs = "This was set in (Shared Resources)/webcontent/qfree/jsjeff.js";
